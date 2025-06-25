fe4fb68d0dfc9e9d4d5d850ab04eb2c4ee5574f7
shreyaghosh19sg <sg694282@gmail.com>
2025-06-25 23:48:59 +0530

    Add files via upload

---
+++ contactForm.js
@@ -0,0 +1,17 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    // Get form values
+    const name = this.elements[0].value;
+    const email = this.elements[1].value;
+    const message = this.elements[2].value;
+    
+    // Here you would typically send the data to a server
+    console.log('Form submitted:', { name, email, message });
+    
+    // Show success message
+    alert('Thank you for your message! I will get back to you soon.');
+    
+    // Reset form
+    this.reset();
+});
